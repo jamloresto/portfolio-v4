bca8b2189978819d0458df146446eba9a833fe98
jamloresto <101079813+jamloresto@users.noreply.github.com>
2025-10-09 15:35:03 +0800

    add base index page (#6)

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="apple-touch-icon" sizes="180x180" href="./assets/images/favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="./assets/images/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="./assets/images/favicon/favicon-16x16.png">
+	<link rel="manifest" href="./assets/images/favicon/site.webmanifest">
+
+	<title>Jam Works | Portfolio</title>
+	
+	<link rel="stylesheet" href="./assets/css/build.css">
+
+</head>
+
+<body>
+	<main class="main-wrapper">
+		<div id="smooth-wrapper">
+			<div id="smooth-content">
+				<?php include 'assets/components/navigation.php'; ?>
+			</div>
+		</div>
+	</main>
+
+	<!-- vendor -->
+	<script src="/assets/js/vendor/jquery.min.js"></script>
+	<script src="/assets/js/vendor/gsap.min.js"></script>
+	<script src="/assets/js/vendor/ScrollTrigger.min.js"></script>
+	<script src="/assets/js/vendor/ScrollToPlugin.min.js"></script>
+	<script src="/assets/js/vendor/ScrollSmoother.min.js"></script>
+
+	<!-- custom -->
+	<script src="/assets/js/custom/app.js"></script>
+	<script src="/assets/js/custom/global.js"></script>
+	<script src="/assets/js/custom/script.js"></script>
+	<script src="/assets/js/custom/animation.js"></script>
+
+</body>
+
+</html>
